9f8f30d0e5d78c62983e0b3d7c9bb7436a92e4e0
Lac <laclys@126.com>
2018-08-04 23:56:39 +0800

    feat: add config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: Lac 
+ * @Date: 2018-08-04 23:55:15 
+ * @Last Modified by:   Lac 
+ * @Last Modified time: 2018-08-04 23:55:15 
+ */
+export const config = {
+  api_base_url: 'http://bl.7yue.pro/v1/',
+  appkey: '3Hrmc2eWzeYtWX50'
+}
